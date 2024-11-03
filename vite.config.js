2d29c53b360a5d43dfee5f8ee0ab63c41b7426b3
dhershman1 <dustinh17@gmail.com>
2024-11-03 16:04:15 -0500

    Updated: Frontend to talk to our API

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,27 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
+import { version } from './package.json'
+
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_APP_VERSION__: JSON.stringify(version)
+  },
   plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: 'localhost',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        rewrite: (path) => path.replace(/^\/api\//, '')
+      }
     }
   }
 })
