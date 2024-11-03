07039f0b05e22663cdbb63880bdcdae24910fb8b
dhershman1 <dustinh17@gmail.com>
2024-11-03 16:03:03 -0500

    Added: About View

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>About YuGiBinder</h2>
+    <p>
+      YuGiBinder is a web application that allows users to create binders to store their Yu-Gi-Oh! cards. Users can
+      create, read, update, and delete binders and cards. Users can also create, read, update, and delete decks. Users
+      can add cards to decks and remove cards from decks. Users can also search for cards by name and add cards to
+      binders.
+    </p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.about {
+  margin: auto;
+  max-width: 50%;
+  text-align: center;
 }
 </style>
