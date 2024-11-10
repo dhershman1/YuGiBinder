1b69f5dd09ce7d03c98d7b4bd4b08c393e8fd7c8
dhershman1 <dustinh17@gmail.com>
2024-11-10 02:44:07 -0500

    Updated: Retrieve card to take query params

--- src/stores/cards.js
+++ src/stores/cards.js
@@ -4,6 +4,23 @@ import { ref } from 'vue'
 export const useCardsStore = defineStore('cards', () => {
   const topCards = ref([])
   const currentCard = ref({})
+  const cards = ref([])
+
+  async function retrieveCards(params) {
+    const queryString = new URLSearchParams(params).toString()
+    const url = `/api/cards?${queryString}`
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await response.json()
+
+    return data
+  }
 
   async function retrieveTopCards() {
     if (topCards.value.length > 0) {
@@ -38,7 +55,9 @@ export const useCardsStore = defineStore('cards', () => {
   }
 
   return {
+    cards,
     currentCard,
+    retrieveCards,
     retrieveCardById,
     retrieveTopCards,
     topCards
