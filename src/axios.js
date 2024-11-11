0c319fedf4769c7dab4b5b8fa394c10903fe4560
dhershman1 <dustinh17@gmail.com>
2024-11-11 17:56:12 -0500

    Updated: Have axios use the authorization header instead of cookies

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  },
   withCredentials: true // Include cookies with requests
 })
 
