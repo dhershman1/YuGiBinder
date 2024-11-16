371b4b9fa81dc64e583a060c5564fceee6e1900c
dhershman1 <dustinh17@gmail.com>
2024-11-16 18:11:43 -0500

    ADded: Sentry Setup

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueFeather from 'vue-feather'
 import { MotionPlugin } from '@vueuse/motion'
+import * as Sentry from '@sentry/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -27,5 +28,18 @@ app.use(createPinia())
 app.use(router)
 app.use(MotionPlugin)
 app.use(Auth0)
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration({ router }), Sentry.replayIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost'],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 app.mount('#app')
