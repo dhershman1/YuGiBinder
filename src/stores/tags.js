c0c27bd190195e1ef72f6aa67f4e8d9368d1e301
dhershman1 <dustinh17@gmail.com>
2024-11-19 19:32:39 -0500

    Added: new function to get tags for a specific binder id

--- src/stores/tags.js
+++ src/stores/tags.js
@@ -1,9 +1,11 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { useAuthDependencies } from '@/composables/auth'
+import axiosNoAuth from 'axios'
 
 export const useTagsStore = defineStore('tags', () => {
   const tags = ref([])
+  const currentBinderTags = ref([])
 
   async function retrieveTags() {
     const { axios } = await useAuthDependencies()
@@ -12,8 +14,23 @@ export const useTagsStore = defineStore('tags', () => {
     tags.value = response.data
   }
 
+  async function retrieveTagsForBinder(binderId) {
+    const response = await axiosNoAuth(`/api/tags/${binderId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    currentBinderTags.value = response.data
+
+    return response.data
+  }
+
   return {
+    currentBinderTags,
     retrieveTags,
+    retrieveTagsForBinder,
     tags
   }
 })
