9f1b9a728789ba7a4302a5f4224e7ea27c8adb29
dhershman1 <dustinh17@gmail.com>
2024-11-20 23:30:28 -0500

    Udpated: Rewrote not found view with new card stack animation

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,153 +1,189 @@
+<script setup>
+import { nextTick, onMounted, ref } from 'vue'
+import { useCardsStore } from '@/stores/cards'
+import DotLoader from '@/components/DotLoader.vue'
+
+// Stores
+const cardsStore = useCardsStore()
+// Refs
+const loading = ref(true)
+const cards = ref(null)
+const autoplayInterval = ref(null)
+const stack = ref(null)
+const stackedCards = ref(null)
+
+// JavaScript to do our swapping effect
+function moveCard() {
+  const lastCard = stack.value.lastElementChild
+  if (lastCard.classList.contains('stacked-card')) {
+    lastCard.classList.add('swap')
+
+    setTimeout(() => {
+      lastCard.classList.remove('swap')
+      stack.value.insertBefore(lastCard, stack.value.firstElementChild)
+    }, 1200)
+  }
+}
+
+onMounted(async () => {
+  cards.value = await cardsStore.retrieveRandomCard(5)
+  autoplayInterval.value = setInterval(moveCard, 2500)
+  loading.value = false
+  await nextTick()
+  stack.value = document.querySelector('.stack')
+  stackedCards.value = Array.from(stack.value.children)
+    .reverse()
+    .filter((child) => child.classList.contains('stacked-card'))
+  stackedCards.value.forEach((card) => stack.value.appendChild(card))
+})
+</script>
+
 <template>
-  <div class="not-found">
-    <svg
-      width="380px"
-      height="500px"
-      viewBox="0 0 837 1045"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"
-    >
-      <g
-        id="Page-1"
-        stroke="none"
-        stroke-width="1"
-        fill="none"
-        fill-rule="evenodd"
-        sketch:type="MSPage"
-      >
-        <path
-          d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z"
-          id="Polygon-1"
-          stroke="#007FB2"
-          stroke-width="6"
-          sketch:type="MSShapeGroup"
-        ></path>
-        <path
-          d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z"
-          id="Polygon-2"
-          stroke="#EF4A5B"
-          stroke-width="6"
-          sketch:type="MSShapeGroup"
-        ></path>
-        <path
-          d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z"
-          id="Polygon-3"
-          stroke="#795D9C"
-          stroke-width="6"
-          sketch:type="MSShapeGroup"
-        ></path>
-        <path
-          d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z"
-          id="Polygon-4"
-          stroke="#F2773F"
-          stroke-width="6"
-          sketch:type="MSShapeGroup"
-        ></path>
-        <path
-          d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z"
-          id="Polygon-5"
-          stroke="#36B455"
-          stroke-width="6"
-          sketch:type="MSShapeGroup"
-        ></path>
-      </g>
-    </svg>
-    <div class="message-box">
-      <h1>404</h1>
-      <p>Page not found</p>
-      <div class="action-link-wrap">
-        <a
-          onclick="history.back(-1)"
-          class="btn btn__primary"
-          >Go Back</a
-        >
-        <router-link
-          to="/"
-          class="btn btn__primary"
-          >Go to Home Page
-        </router-link>
+  <div
+    v-if="!loading && cards"
+    class="not-found"
+  >
+    <section class="content">
+      <div class="message-box">
+        <h1>404</h1>
+        <p>Page not found</p>
+        <div class="action-link-wrap">
+          <router-link
+            to="/"
+            class="action-link btn btn__primary"
+            >Go to Home</router-link
+          >
+        </div>
       </div>
-    </div>
+    </section>
+    <section
+      class="stack"
+      @click="moveCard"
+    >
+      <div
+        v-for="card in cards"
+        :key="card.id"
+        class="stacked-card"
+      >
+        <img :src="`https://imgs.yugibinder.com/cards/normal/${card.id}.jpg`" />
+      </div>
+    </section>
   </div>
+  <dot-loader v-else />
 </template>
 
 <style scoped>
 .not-found {
   display: grid;
-  grid-template-columns: 3fr 2fr;
+  grid-template-columns: 2fr 3fr;
 }
 
-svg {
-  margin: auto;
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 
 .message-box {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  font-size: 24px;
 }
 
 .message-box h1 {
-  font-size: 60px;
-  line-height: 46px;
-  margin-bottom: 40px;
+  font-size: 10rem;
+  margin: 0;
 }
 
-.action-link-wrap {
-  display: flex;
-  gap: 1rem;
+.stack {
+  position: relative;
 }
 
-#Polygon-1,
-#Polygon-2,
-#Polygon-3,
-#Polygon-4,
-#Polygon-4,
-#Polygon-5 {
-  animation: float 1s infinite ease-in-out alternate;
-}
-#Polygon-2 {
-  animation-delay: 0.2s;
-}
-#Polygon-3 {
-  animation-delay: 0.4s;
-}
-#Polygon-4 {
-  animation-delay: 0.6s;
-}
-#Polygon-5 {
-  animation-delay: 0.8s;
+.stacked-card {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  width: 350px;
+  height: 500px;
+  box-shadow:
+    0 5px 10px 0 rgba(0, 0, 0, 0.25),
+    0 15px 20px 0 rgba(0, 0, 0, 0.125);
+  transition: transform 0.6s;
+  user-select: none;
 }
 
-@keyframes float {
+.stacked-card img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: inherit;
+  object-fit: cover;
+  pointer-events: none;
+}
+
+.stacked-card:nth-last-child(n + 5) {
+  --x: calc(-50% + 90px);
+  transform: translate(var(--x), -50%) scale(0.85);
+  box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.01);
+}
+
+.stacked-card:nth-last-child(4) {
+  --x: calc(-50% + 60px);
+  transform: translate(var(--x), -50%) scale(0.9);
+}
+
+.stacked-card:nth-last-child(3) {
+  --x: calc(-50% + 30px);
+  transform: translate(var(--x), -50%) scale(0.95);
+}
+
+.stacked-card:nth-last-child(2) {
+  --x: calc(-50%);
+  transform: translate(var(--x), -50%) scale(1);
+}
+
+.stacked-card:nth-last-child(1) {
+  --x: calc(-50% - 30px);
+  transform: translate(var(--x), -50%) scale(1.05);
+}
+
+.stacked-card:nth-last-child(1) img {
+  box-shadow: 0 1px 5px 5px rgba(255, 193, 111, 0.5);
+}
+
+.swap {
+  animation: swap 1.3s ease-out forwards;
+}
+
+@keyframes swap {
+  30% {
+    transform: translate(calc(var(--x) - 250px), -50%) scale(0.85) rotate(-5deg) rotateY(65deg);
+  }
   100% {
-    transform: translateY(20px);
+    transform: translate(calc(var(--x) - 30px), -50%) scale(0.5);
+    z-index: -1;
   }
 }
-@media (max-width: 450px) {
+
+@media (max-width: 768px) {
   .not-found {
     grid-template-columns: 1fr;
   }
 
-  svg {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-top: -250px;
-    margin-left: -190px;
-  }
-  .message-box {
-    position: absolute;
-    top: 50%;
-    left: 65%;
-    margin-top: -100px;
-    margin-left: -190px;
-    text-align: center;
-  }
   .action-link-wrap {
-    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  .action-link {
+    padding: 1.5rem;
+  }
+
+  .stack {
+    display: none;
   }
 }
 </style>
